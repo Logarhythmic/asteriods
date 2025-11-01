66da6a7922252b1dd1a5d46495fc07ab2851d278
Logarhythmic <akaihige88@gmail.com>
2025-11-01 11:48:58 -0400

    drew player to screen

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
-    
-    #print(f'Screen width: {SCREEN_WIDTH}')
-    #print(f'Screen height: {SCREEN_HEIGHT}')
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    #print(f'Asteroid min radius: {ASTEROID_MIN_RADIUS}')
-    #print(f'Asteroid kinds: {ASTEROID_KINDS}')
-    #print(f'Asteroid spawn rate: {ASTEROID_SPAWN_RATE}')
-    #print(f'Asteroid max radius: {ASTEROID_MAX_RADIUS}')
+    game_clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
 
     while True:
         for event in pygame.event.get():
@@ -19,9 +18,11 @@ def main():
                 pygame.quit()
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
 
         #always last
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
